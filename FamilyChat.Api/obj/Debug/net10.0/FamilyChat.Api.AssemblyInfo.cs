226b1662ce5d877b150c25160d32dd42f7df9bec
Batihan Eyyubi <batocode@gmail.com>
2026-09-02 10:07:20 +0300

    fix railway build

--- FamilyChat.Api/obj/Debug/net10.0/FamilyChat.Api.AssemblyInfo.cs
+++ FamilyChat.Api/obj/Debug/net10.0/FamilyChat.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamilyChat.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+203afde63e05fff5ccf2a66ef8914df964bac84f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afc4f72fde92d5138ffb12d136024e158024522d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamilyChat.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamilyChat.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
